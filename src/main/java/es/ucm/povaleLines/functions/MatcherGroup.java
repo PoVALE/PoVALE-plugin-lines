1a2c0a3b9468b970ea48b32f800a478e910b5d2b
drossetto <rosselow@gmail.com>
2017-05-05 11:47:53 +0200

    update

--- src/main/java/es/ucm/povaleLines/functions/MatcherGroup.java
+++ src/main/java/es/ucm/povaleLines/functions/MatcherGroup.java
@@ -6,6 +6,7 @@
 package es.ucm.povaleLines.functions;
 
 import es.ucm.povale.annotation.CallableMethod;
+import es.ucm.povale.annotation.ParamDescription;
 import es.ucm.povale.entity.IntegerEntity;
 import es.ucm.povale.entity.StringEntity;
 import es.ucm.povale.function.Function;
@@ -16,19 +17,26 @@ import es.ucm.povaleFiles.entities.MatchResult;
  * @author manuel
  */
 public class MatcherGroup extends Function {
-
+    private String message;
     @Override
     public String getName() {
         return "match-group";
     }
     
     @CallableMethod
-    public StringEntity match(MatchResult mr, IntegerEntity i) {
+    public StringEntity match(@ParamDescription("")MatchResult mr, IntegerEntity i) {
         if (i.getValue() >= 1 && i.getValue() <= mr.getGroups().size()) {
-            return new StringEntity(mr.getGroups().get(i.getValue() - 1));
+            message = mr.getGroups().get(i.getValue() - 1);
+            return new StringEntity(message);
         } else {
-            return new StringEntity("");
+            message="";
+            return new StringEntity(message);
         }
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
     
 }
